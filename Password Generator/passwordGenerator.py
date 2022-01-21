779b999ff86a41a993bcf6b88bf7664f68bd30ed
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2022-01-21 18:41:12 +0100

    Update passwordGenerator.py

--- Password Generator/passwordGenerator.py
+++ Password Generator/passwordGenerator.py
@@ -1,4 +1,5 @@
 #Password Generator Project
+#random använder Merseme Twiser som generar random numbers :) 
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -32,6 +33,6 @@ while i < lenght_password:
     
 print(password)
 
-
+#Alternativ 2 3 for loops spara i lista och använda random.shuffle(list) :) 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
